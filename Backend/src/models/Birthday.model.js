79126d4d6cddc5dcffe2fe5ce4b3fcd13c288be8
Navesh Jaiswal <navesh.professional@gmail.com>
2025-07-18 14:01:16 +0530

    rename model

---
+++ Backend/src/models/Birthday.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const birthdaySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  dob: { type: Date, required: true },
+  email: { type: String, required: true },
+  reminder: { type: Boolean, default: true },
+});
+
+const Birthday = mongoose.model("Birthday", birthdaySchema);
+export default Birthday;
